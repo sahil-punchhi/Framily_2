59bc50a29f2248015e0afbaa415a020b5f4fc5ce
sahil-punchhi <sahil.punchhi@gmail.com>
2021-01-03 16:40:52 +1100

    add client folder

---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import matches from './matches';
+
+export default combineReducers({
+  matches,
+});
---
+++ client/src/reducers/matches.js
@@ -0,0 +1,8 @@
+const matchReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SOMETHING':
+      return state;
+    default:
+      return state;
+  }
+};
